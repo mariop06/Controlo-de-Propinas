ba2433746369bb9431df6192e5f274e91ac8faa3
Mário Paulo <mariopaulobunga@gmail.com>
2024-03-21 19:11:01 +0100

    confirmacao

--- Telas/TelaPrincipal.Designer.cs
+++ Telas/TelaPrincipal.Designer.cs
@@ -31,24 +31,14 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
             this.sideBarP = new Guna.UI.WinForms.GunaPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.labelNome = new System.Windows.Forms.Label();
-            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
-            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
-            this.mainPanel = new System.Windows.Forms.Panel();
             this.panelOpcoes = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.timerButtonMore = new System.Windows.Forms.Timer(this.components);
-            this.label7 = new System.Windows.Forms.Label();
-            this.picUser = new Guna.UI.WinForms.GunaPictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
@@ -56,13 +46,22 @@
             this.btn_more = new Guna.UI.WinForms.GunaAdvenceButton();
             this.btn_info = new Guna.UI.WinForms.GunaAdvenceButton();
             this.gunaPictureBox6 = new Guna.UI.WinForms.GunaPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btn_atividade = new Guna.UI.WinForms.GunaAdvenceButton();
             this.btn_Inicio = new Guna.UI.WinForms.GunaAdvenceButton();
+            this.label4 = new System.Windows.Forms.Label();
             this.btn_dash = new Guna.UI.WinForms.GunaAdvenceButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.timerButtonMore = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.picUser = new Guna.UI.WinForms.GunaPictureBox();
             this.sideBarP.SuspendLayout();
             this.panelOpcoes.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
@@ -71,6 +70,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
             this.SuspendLayout();
             // 
             // sideBarP
@@ -95,73 +95,6 @@
             this.sideBarP.TabIndex = 0;
             this.sideBarP.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPrincipal_Paint);
             this.sideBarP.MouseHover += new System.EventHandler(this.sideBarP_MouseHover);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(6, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "__________";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(7, 729);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "__________";
-            // 
-            // label3
-            // 
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(138, 70);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1320, 21);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "_________________________________________________________________________________" +
-    "________________________________________________________________________________" +
-    "_______";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // labelNome
-            // 
-            this.labelNome.AutoSize = true;
-            this.labelNome.BackColor = System.Drawing.Color.Transparent;
-            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNome.Location = new System.Drawing.Point(1135, 31);
-            this.labelNome.MaximumSize = new System.Drawing.Size(300, 30);
-            this.labelNome.Name = "labelNome";
-            this.labelNome.Size = new System.Drawing.Size(153, 29);
-            this.labelNome.TabIndex = 4;
-            this.labelNome.Text = "Mario Paulo";
-            // 
-            // gunaElipse1
-            // 
-            this.gunaElipse1.Radius = 15;
-            this.gunaElipse1.TargetControl = this;
-            // 
-            // sidebarTimer
-            // 
-            this.sidebarTimer.Interval = 80;
-            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
-            // 
-            // mainPanel
-            // 
-            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainPanel.BackColor = System.Drawing.Color.White;
-            this.mainPanel.Location = new System.Drawing.Point(103, 94);
-            this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(1402, 800);
-            this.mainPanel.TabIndex = 5;
             // 
             // panelOpcoes
             // 
@@ -191,17 +124,6 @@
             this.panel2.Size = new System.Drawing.Size(233, 148);
             this.panel2.TabIndex = 0;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(123, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Turma";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -211,47 +133,6 @@
             this.label5.Size = new System.Drawing.Size(73, 18);
             this.label5.TabIndex = 2;
             this.label5.Text = "Entidade";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(124, 10);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 18);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Aluno";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // timerButtonMore
-            // 
-            this.timerButtonMore.Tick += new System.EventHandler(this.timerButtonMore_Tick);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Silver;
-            this.label7.Location = new System.Drawing.Point(1141, 62);
-            this.label7.MaximumSize = new System.Drawing.Size(300, 30);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(90, 17);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "@mariop06";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
-            // picUser
-            // 
-            this.picUser.BackgroundImage = global::ControloDePropinas.Properties.Resources.user;
-            this.picUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picUser.BaseColor = System.Drawing.Color.White;
-            this.picUser.Location = new System.Drawing.Point(1366, 21);
-            this.picUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.picUser.Name = "picUser";
-            this.picUser.Size = new System.Drawing.Size(57, 49);
-            this.picUser.TabIndex = 2;
-            this.picUser.TabStop = false;
             // 
             // pictureBox5
             // 
@@ -276,6 +157,17 @@
             this.pictureBox6.TabIndex = 6;
             this.pictureBox6.TabStop = false;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 18);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Aluno";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
             // pictureBox7
             // 
             this.pictureBox7.BackgroundImage = global::ControloDePropinas.Properties.Resources.mais;
@@ -286,6 +178,17 @@
             this.pictureBox7.Size = new System.Drawing.Size(22, 18);
             this.pictureBox7.TabIndex = 7;
             this.pictureBox7.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Turma";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // pictureBox3
             // 
@@ -412,6 +315,18 @@
             this.gunaPictureBox6.TabIndex = 1;
             this.gunaPictureBox6.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(6, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "__________";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // btn_atividade
             // 
             this.btn_atividade.AnimationHoverSpeed = 0.07F;
@@ -481,6 +396,17 @@
             this.btn_Inicio.MouseHover += new System.EventHandler(this.btn_Inicio_MouseHover);
             this.btn_Inicio.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_Inicio_MouseUp);
             // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(7, 729);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "__________";
+            // 
             // btn_dash
             // 
             this.btn_dash.AnimationHoverSpeed = 0.07F;
@@ -515,6 +441,80 @@
             this.btn_dash.Click += new System.EventHandler(this.btn_dash_Click);
             this.btn_dash.MouseHover += new System.EventHandler(this.btn_dash_MouseHover);
             // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(138, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1320, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "_______";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.BackColor = System.Drawing.Color.Transparent;
+            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNome.Location = new System.Drawing.Point(1135, 31);
+            this.labelNome.MaximumSize = new System.Drawing.Size(300, 30);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(153, 29);
+            this.labelNome.TabIndex = 4;
+            this.labelNome.Text = "Mario Paulo";
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.Radius = 15;
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Interval = 80;
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackColor = System.Drawing.Color.White;
+            this.mainPanel.Location = new System.Drawing.Point(103, 94);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1402, 800);
+            this.mainPanel.TabIndex = 5;
+            // 
+            // timerButtonMore
+            // 
+            this.timerButtonMore.Tick += new System.EventHandler(this.timerButtonMore_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Silver;
+            this.label7.Location = new System.Drawing.Point(1141, 62);
+            this.label7.MaximumSize = new System.Drawing.Size(300, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "@mariop06";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // picUser
+            // 
+            this.picUser.BackgroundImage = global::ControloDePropinas.Properties.Resources.user;
+            this.picUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picUser.BaseColor = System.Drawing.Color.White;
+            this.picUser.Location = new System.Drawing.Point(1366, 21);
+            this.picUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(57, 49);
+            this.picUser.TabIndex = 2;
+            this.picUser.TabStop = false;
+            // 
             // TelaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -540,7 +540,6 @@
             this.panelOpcoes.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
@@ -549,6 +548,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
